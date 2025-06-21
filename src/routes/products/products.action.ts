3ac85889c260bb03bb2e8d7abb35458c04737e0f
SebastienGras <sebastien.gras75@gmail.com>
2025-06-21 21:11:07 +0200

    feat(products): getProductById

--- src/routes/products/products.action.ts
+++ src/routes/products/products.action.ts
@@ -6,6 +6,7 @@ import type { Request, Response } from "express";
 import { PRODUCTS_ERRORS } from "./products.constant";
 import {
   addProductService,
+  getProductByIdService,
   getProductsByUserIdService,
   getProductsListingService,
   updateProductService,
@@ -113,6 +114,32 @@ export const getProductsByUserIdAction = async (
     });
 
     res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getProductByIdAction = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  logger.info("🔍 Retrieving product by ID:", {
+    productId: req.params.productId,
+  });
+  try {
+    const product = await getProductByIdService(req.params.productId);
+
+    logger.info("✅ Product retrieved successfully:", product);
+    res.json(product);
+  } catch (error) {
+    logger.error("❌ Error while retrieving product:", {
+      error: error instanceof Error ? error.message : String(error),
+      stack: error instanceof Error ? error.stack : undefined,
+    });
+    if (isErrorWithMessage(error, PRODUCTS_ERRORS.PRODUCT_NOT_FOUND)) {
+      res.status(404).json({ error: PRODUCTS_ERRORS.PRODUCT_NOT_FOUND });
+      return;
+    }
+    res.status(500).json({ error: "Internal Server Error" });
+    return;
   }
 };
 
